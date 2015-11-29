d053bafb273f798b649137aeb2b633317555b73e
archit rathi <architrathi10@gmail.com>
2015-11-28 22:33:16 -0500

    fixed UI and basic soundcloud issues

--- popup.js
+++ popup.js
@@ -15,32 +15,38 @@ function displayResults(){
 	tracks = loadIt("http://api.soundcloud.com/tracks.json?client_id=44c88b31e709231ae8f61adc66908de3&q="+que+"&format=json");
 	tracks = '{"songs":'+tracks+'}';
 	obj = JSON.parse(tracks);
-	output+='<button type="button" id="test1">'+obj.songs[0].title+'</button><br>';
-	output+='<button type="button" id="test2">'+obj.songs[1].title+'</button><br>';
-	output+='<button type="button" id="test3">'+obj.songs[2].title+'</button><br>';
-	output+='<button type="button" id="test4">'+obj.songs[3].title+'</button><br>';
-	output+='<button type="button" id="test5">'+obj.songs[4].title+'</button><br>';
+	var img = document.getElementById("img");
+	output+='<button type="button"  id="test1">'+obj.songs[0].title+'</button><br>';
+	output+='<button type="button"  id="test2">'+obj.songs[1].title+'</button><br>';
+	output+='<button type="button"  id="test3">'+obj.songs[2].title+'</button><br>';
+	output+='<button type="button"  id="test4">'+obj.songs[3].title+'</button><br>';
+	output+='<button type="button"  id="test5">'+obj.songs[4].title+'</button><br>';
 	document.getElementById('result').innerHTML = output;
 
 	document.getElementById('test1').onclick = function(){
 		bg.st(obj.songs[0].id);
+		img.src=obj.songs[0].artwork_url;
 	};
 
 
 	document.getElementById('test2').onclick = function(){
 		bg.st(obj.songs[1].id);
+		img.src=obj.songs[0].artwork_url;
 	};
 
 	document.getElementById('test3').onclick = function(){
 		bg.st(obj.songs[2].id);
+		img.src=obj.songs[2].artwork_url;
 	};
 
 	document.getElementById('test4').onclick = function(){
 		bg.st(obj.songs[3].id);
+		img.src=obj.songs[3].artwork_url;
 	};
 
 	document.getElementById('test5').onclick = function(){
 		bg.st(obj.songs[4].id);
+		img.src=obj.songs[4].artwork_url;
 	};
 
 
@@ -53,6 +59,7 @@ function yes(){
 
 function no(){
 	bg.sto();
+	
 };
 
 
